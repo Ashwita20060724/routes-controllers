a5fc1e7f0d062ec4a4d7bd1fd308b44bf3d6e8e6
Ashwita20060724 <ashwitasuresh@gmail.com>
2026-02-25 12:15:36 +0100

    finalizacion  del laboratorio 2

--- DeliverUS-Backend/src/controllers/RestaurantController.js
+++ DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -22,18 +22,78 @@ const index = async function (req, res) {
 // TODO: Complete the following functions
 
 const create = async function (req, res) {
-
+  try {
+    const restaurant = await Restaurant.create(req.body)
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
+/* La funcion show se encarga de buscar un restaurante por su id,
+ */
 const show = async function (req, res) {
-
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId,
+      {
+        attributes: { exclude: ['userId'] },
+        include: [{
+          model: Product,
+          as: 'products',
+          include: {
+            model: ProductCategory,
+            as: 'productCategory'
+          }
+        },
+        {
+          model: RestaurantCategory,
+          as: 'restaurantCategory'
+        }],
+        order: [[{ model: Product, as: 'products' },
+          'order', 'ASC']]
+      })
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const update = async function (req, res) {
+  try {
+    // 1. Actualizamos el restaurante
+    await Restaurant.update(req.body, {
+      where: { id: req.params.restaurantId }
+    })
 
+    // 2. Buscamos de nuevo el restaurante actualizado
+    const updatedRestaurant = await Restaurant.findByPk(
+      req.params.restaurantId
+    )
+
+    // 3. Devolvemos el restaurante actualizado
+    res.json(updatedRestaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const destroy = async function (req, res) {
+  try {
+    // destroy solo devuelve cuantas filas han sido eliminadas, no el objeto eliminado
+    const deletedRows = await Restaurant.destroy({
+      where: { id: req.params.restaurantId }
+    })
+
+    // Si no se ha eliminado ninguna fila, es porque el restaurante no existe
+    if (deletedRows === 0) {
+      return res.status(404).json({ error: 'Restaurant not found' })
+    }
+
+    // Si se ha eliminado el restaurante, devolvemos un mensaje de información
+    res.json({ message: 'Restaurant deleted successfully' })
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const RestaurantController = {
--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -2,6 +2,12 @@ import RestaurantController from '../controllers/RestaurantController.js'
 
 const loadFileRoutes = function (app) {
   // TODO: Include routes for restaurant described in the lab session README
-
+  app.route('/restaurants')
+    .post(RestaurantController.create)
+    .get(RestaurantController.index)
+  app.route('/restaurants/:restaurantId')
+    .get(RestaurantController.show)
+    .put(RestaurantController.update)
+    .delete(RestaurantController.delete)
 }
 export default loadFileRoutes
